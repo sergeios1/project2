e9301c74f58e346121aadf5ea11b1ed68dfd140b
sergios1 <sergetnazaretyan@gmail.com>
2021-01-10 16:24:22 -0800

    a

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get("/api/hello", function (req, res) {
 
 
 app.get("/api/whoami", (req, res) => {
-  res.json({ipaddress: "99.119.251.92"});
+  res.json({ipaddress: "99.119.251.92", language: "en-US,en;q=0.5"});
 });
 
 
